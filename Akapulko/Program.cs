2fd1924efd15505765c4fb55f1d126fb65c8a86f
Przemek <kromek25@gmail.com>
2017-10-27 14:12:59 +0200

    test

--- Akapulko/Program.cs
+++ Akapulko/Program.cs
@@ -17,7 +17,6 @@ namespace Akapulko
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            cout<<"Jajko";
         }
     }
 }
