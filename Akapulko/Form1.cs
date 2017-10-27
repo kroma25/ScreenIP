06c312a38c135dab44f9ae577896c6ce6010817e
Przemek <kromek25@gmail.com>
2017-10-27 14:04:10 +0200

    Dodany textbox

--- Akapulko/Form1.cs
+++ Akapulko/Form1.cs
@@ -16,5 +16,10 @@ namespace Akapulko
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
